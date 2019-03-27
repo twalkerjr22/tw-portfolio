94a19850ad5e626e2017cd5597bce34456a614a3
Troy Walker <twalkerjr22@gmail.com>
2019-03-27 15:37:29 -0400

    Updates

--- precache-manifest.e80a206fa9b5df4eba1acbc865b0d7b0.js
+++ precache-manifest.e80a206fa9b5df4eba1acbc865b0d7b0.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/tw-portfolio/static/js/runtime~main.0ce06eb4.js"
   },
   {
-    "revision": "a38c64d2a1543928f279",
-    "url": "/tw-portfolio/static/js/main.b960b7f6.chunk.js"
+    "revision": "b8f93b19b668bcedc558",
+    "url": "/tw-portfolio/static/js/main.5a3349b2.chunk.js"
   },
   {
-    "revision": "0cbf1d6f7e5b7be35282",
-    "url": "/tw-portfolio/static/js/2.69cc6540.chunk.js"
+    "revision": "deec0fccfd3595cb1aee",
+    "url": "/tw-portfolio/static/js/2.284fe6d0.chunk.js"
   },
   {
-    "revision": "a38c64d2a1543928f279",
-    "url": "/tw-portfolio/static/css/main.1e68ed81.chunk.css"
+    "revision": "b8f93b19b668bcedc558",
+    "url": "/tw-portfolio/static/css/main.248de884.chunk.css"
   },
   {
-    "revision": "0cbf1d6f7e5b7be35282",
+    "revision": "deec0fccfd3595cb1aee",
     "url": "/tw-portfolio/static/css/2.673c6054.chunk.css"
   },
   {
-    "revision": "2de988c83c6d6703254e4ec4683f4eec",
+    "revision": "4d60f19352b7d90fcf3f74e5a4d4d10e",
     "url": "/tw-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tw-portfolio/precache-manifest.bee1a9627f35c1f2c56cd9c47d5236dc.js"
+  "/tw-portfolio/precache-manifest.e80a206fa9b5df4eba1acbc865b0d7b0.js"
 );
 
 workbox.clientsClaim();
