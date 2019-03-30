d375197da8dbb1b22b3c1f1ce5b5694a3d08d6f3
Troy Walker <twalkerjr22@gmail.com>
2019-03-30 16:54:48 -0400

    Updates

--- precache-manifest.7d34f6e1f3299e984e8263e8e75976f8.js
+++ precache-manifest.7d34f6e1f3299e984e8263e8e75976f8.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/tw-portfolio/static/media/whha.b31aaa45.png"
   },
   {
-    "revision": "0e9791a78de83f872657",
-    "url": "/tw-portfolio/static/css/main.c3d3d2ff.chunk.css"
+    "revision": "2b022c1b9d5ed6c48f51",
+    "url": "/tw-portfolio/static/css/main.73ff69a9.chunk.css"
   },
   {
     "revision": "9329ff1896b168f113e8",
@@ -16,28 +16,28 @@ self.__precacheManifest = [
     "url": "/tw-portfolio/static/media/sba-log.bfb5efcf.png"
   },
   {
-    "revision": "a5f37d20b18054646dbe",
-    "url": "/tw-portfolio/static/js/2.c0b25763.chunk.js"
+    "revision": "46cdb2beaa389fd703e6",
+    "url": "/tw-portfolio/static/js/2.6592f790.chunk.js"
   },
   {
     "revision": "d839188883f1d9f624337537c91f8360",
     "url": "/tw-portfolio/static/media/hero.d8391888.png"
   },
   {
-    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
-    "url": "/tw-portfolio/static/media/react-logo.47ce6e77.png"
+    "revision": "bd0ef7a8e6f532c7f1722bf9003bbabe",
+    "url": "/tw-portfolio/static/media/ab-icon.bd0ef7a8.png"
   },
   {
-    "revision": "0e9791a78de83f872657",
-    "url": "/tw-portfolio/static/js/main.fef4b4af.chunk.js"
+    "revision": "2b022c1b9d5ed6c48f51",
+    "url": "/tw-portfolio/static/js/main.08f3da37.chunk.js"
   },
   {
     "revision": "2bf953334a0c14c2dce86bad298df04c",
     "url": "/tw-portfolio/static/media/logo.2bf95333.png"
   },
   {
-    "revision": "bd0ef7a8e6f532c7f1722bf9003bbabe",
-    "url": "/tw-portfolio/static/media/ab-icon.bd0ef7a8.png"
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/tw-portfolio/static/media/react-logo.47ce6e77.png"
   },
   {
     "revision": "fb77e15c96754d9d3c1bec6f0593844b",
@@ -48,11 +48,11 @@ self.__precacheManifest = [
     "url": "/tw-portfolio/static/media/mslogo.a864781c.png"
   },
   {
-    "revision": "a5f37d20b18054646dbe",
+    "revision": "46cdb2beaa389fd703e6",
     "url": "/tw-portfolio/static/css/2.673c6054.chunk.css"
   },
   {
-    "revision": "f5e8e693d5d7f1cac46d202043af2275",
+    "revision": "f7087ad988846c92c694ce37662e812e",
     "url": "/tw-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tw-portfolio/precache-manifest.62e0dd93ecc753134a0b05cb1260db91.js"
+  "/tw-portfolio/precache-manifest.7d34f6e1f3299e984e8263e8e75976f8.js"
 );
 
 workbox.clientsClaim();
